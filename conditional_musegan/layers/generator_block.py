e2e66d889100f207f2e6972595627bc048326710
KateSawada <sawapy630@gmail.com>
2023-03-10 18:40:02 +0900

    generator #1

--- conditional_musegan/layers/generator_block.py
+++ conditional_musegan/layers/generator_block.py
@@ -8,23 +8,35 @@ import torch
 
 
 class GeneratorBlock(torch.nn.Module):
-    def __init__(self, in_dim, out_dim, kernel, stride):
+    def __init__(self, in_dim, out_dim, kernel, stride, activation="relu"):
+        """Generator Block
+
+        Args:
+            in_dim (int): input channels
+            out_dim (int): output channels
+            kernel (array like): kernel size. Must has three elements
+            stride (array like): stride. Must has three elements
+            activation (str, optional):
+                activation function. "relu", "leaky_relu", "sigmoid", "tanh"
+                and "x" are available. Defaults to "relu".
+        """
         super().__init__()
         self.transconv = torch.nn.ConvTranspose3d(in_dim, out_dim, kernel, stride)
         self.batchnorm = torch.nn.BatchNorm3d(out_dim)
+        if activation == "relu":
+            self.activation = torch.nn.functional.relu
+        elif activation == "leaky_relu":
+            self.activation = torch.nn.functional.leaky_relu
+        elif activation == "sigmoid":
+            self.activation = torch.sigmoid
+        elif activation == "tanh":
+            self.activation = torch.tanh
+        elif activation == "x":
+            self.activation = lambda x: x  # f(x) = x
+        else:
+            raise ValueError(f"{activation} is not supported")
 
     def forward(self, x):
         x = self.transconv(x)
         x = self.batchnorm(x)
-        return torch.nn.functional.relu(x)
-
-class FinalGeneratorBlock(torch.nn.Module):
-    def __init__(self, in_dim, out_dim, kernel, stride):
-        super().__init__()
-        self.transconv = torch.nn.ConvTranspose3d(in_dim, out_dim, kernel, stride)
-        self.batchnorm = torch.nn.BatchNorm3d(out_dim)
-
-    def forward(self, x):
-        x = self.transconv(x)
-        x = self.batchnorm(x)
-        return x
+        return self.activation(x)
--- conditional_musegan/models/generator.py
+++ conditional_musegan/models/generator.py
@@ -9,7 +9,7 @@ from logging import getLogger
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from conditional_musegan.layers import GeneratorBlock, FinalGeneratorBlock
+from conditional_musegan.layers import GeneratorBlock
 
 # A logger for this file
 logger = getLogger(__name__)
@@ -49,10 +49,10 @@ class MuseganGenerator(torch.nn.Module):
         # self.conditioning_dim = conditioning_dim
 
         shared_conv_params = {
-            "in_channel": [latent_dim, 256, 128, 64],
-            "out_channel": [256, 128, 64, 32],
-            "kernel": [(4, 1, 1), (1, 4, 1), (1, 1, 4), (1, 1, 3)],
-            "stride": [(4, 1, 1), (1, 4, 1), (1, 1, 4), (1, 1, 1)],
+            "in_channel": [latent_dim, 512, 256],
+            "out_channel": [512, 256, 128],
+            "kernel": [(4, 1, 1), (1, 4, 3), (1, 4, 3)],
+            "stride": [(4, 1, 1), (1, 4, 3), (1, 4, 2)],
         }
         self.shared_conv_network = nn.ModuleList()
         for i in range(len(shared_conv_params["in_channel"])):
@@ -62,16 +62,70 @@ class MuseganGenerator(torch.nn.Module):
                     shared_conv_params["out_channel"][i],
                     shared_conv_params["kernel"][i],
                     shared_conv_params["stride"][i],
+                    "relu",
                 )
             ]
 
+        # pitch-time private
+        pitch_time_params = {
+            "in_channel": [128, 32],
+            "out_channel": [32, 16],
+            "kernel": [(1, 1, 12), (1, 3, 1)],
+            "stride": [(1, 1, 12), (1, 3, 1)],
+        }
+        self.pitch_time_private = nn.ModuleList()
+        for i in range(len(pitch_time_params["in_channel"])):
+            self.pitch_time_private += [
+                nn.ModuleList(
+                    [
+                        GeneratorBlock(
+                            pitch_time_params["in_channel"][i],
+                            pitch_time_params["out_channel"][i],
+                            pitch_time_params["kernel"][i],
+                            pitch_time_params["stride"][i],
+                            "relu",
+                        )
+                        for _ in range(self.n_tracks)
+                    ]
+                )
+            ]
+
+        # time-pitch private
+        time_pitch_params = {
+            "in_channel": [128, 32],
+            "out_channel": [32, 16],
+            "kernel": [(1, 3, 1), (1, 1, 12)],
+            "stride": [(1, 3, 1), (1, 1, 12)],
+        }
+        self.time_pitch_private = nn.ModuleList()
+        for i in range(len(time_pitch_params["in_channel"])):
+            self.time_pitch_private += [
+                nn.ModuleList(
+                    [
+                        GeneratorBlock(
+                            time_pitch_params["in_channel"][i],
+                            time_pitch_params["out_channel"][i],
+                            time_pitch_params["kernel"][i],
+                            time_pitch_params["stride"][i],
+                            "relu",
+                        )
+                        for _ in range(self.n_tracks)
+                    ]
+                )
+            ]
+
+        assert len(self.time_pitch_private) == len(self.pitch_time_private), \
+            "pitch-time and time-pitch must have same number of layers."
+
+        # merged private
         private_conv_params = {
-            "in_channel": [32, 16],
-            "out_channel": [16, 1],
-            "kernel": [(1, 4, 1), (1, 1, 12)],
-            "stride": [(1, 4, 1), (1, 1, 12)],
+            "in_channel": [32],
+            "out_channel": [1],
+            "kernel": [(1, 1, 1)],
+            "stride": [(1, 1, 1)],
         }
         self.private_conv_network = nn.ModuleList()
+        # this loop will be not executed because number of private conv is 1
         for i in range(len(private_conv_params["in_channel"]) - 1):
             self.private_conv_network += [
                 nn.ModuleList(
@@ -81,23 +135,27 @@ class MuseganGenerator(torch.nn.Module):
                             private_conv_params["out_channel"][i],
                             private_conv_params["kernel"][i],
                             private_conv_params["stride"][i],
+                            "relu"
                         )
                         for _ in range(self.n_tracks)
                     ]
                 )
             ]
+
+        # final layers use tanh as  activation function
         self.private_conv_network += [
             nn.ModuleList(
-                    [
-                        FinalGeneratorBlock(
-                            private_conv_params["in_channel"][-1],
-                            private_conv_params["out_channel"][-1],
-                            private_conv_params["kernel"][-1],
-                            private_conv_params["stride"][-1],
-                        )
-                        for _ in range(self.n_tracks)
-                    ]
-                )
+                [
+                    GeneratorBlock(
+                        private_conv_params["in_channel"][-1],
+                        private_conv_params["out_channel"][-1],
+                        private_conv_params["kernel"][-1],
+                        private_conv_params["stride"][-1],
+                        "tanh"
+                    )
+                    for _ in range(self.n_tracks)
+                ]
+            )
         ]
 
     def forward(self, x):
@@ -115,12 +173,22 @@ class MuseganGenerator(torch.nn.Module):
         for i in range(len(self.shared_conv_network)):
             x = self.shared_conv_network[i](x)
 
-        # private (first)
-        x = [conv(x) for conv in self.private_conv_network[0]]
-        # private (second to final)
-        for i in range(1, len(self.private_conv_network)):
+        # time-pitch and pitch-time
+        pt = [conv(x) for conv in self.pitch_time_private[0]]
+        tp = [conv(x) for conv in self.time_pitch_private[0]]
+
+        for i_layer in range(1, len(self.time_pitch_private)):
+            pt = [conv(x_) for x_, conv in zip(pt, self.pitch_time_private[i_layer])]
+            tp = [conv(x_) for x_, conv in zip(tp, self.time_pitch_private[i_layer])]
+
+        # merge pitch-time and time-pitch
+        x = [torch.cat((pt[i], tp[i]), 1) for i in range(self.n_tracks)]
+
+        # merged private
+        for i in range(len(self.private_conv_network)):
             x = [conv(x_) for x_, conv in zip(x, self.private_conv_network[i])]
+
+        # reshape
         x = torch.cat(x, 1)
         x = x.view(-1, self.n_tracks, self.n_measures * self.measure_resolution, self.n_pitches)
-        x = torch.sigmoid(x)
         return x
